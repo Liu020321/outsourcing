6fe4ace8d5829493dfc4ebbd85dab6d440f6c211
LZJ20020810 <1214068621@qq.com>
2023-04-02 23:58:05 +0800

    实现了跳转html又到servlet界面的功能

--- src/main/java/com/outsourcing/web/InsertPictureServlet.java
+++ src/main/java/com/outsourcing/web/InsertPictureServlet.java
@@ -1,11 +1,13 @@
 package com.outsourcing.web;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/InsertPictureServlet")
 public class InsertPictureServlet extends HttpServlet {
 
     @Override
