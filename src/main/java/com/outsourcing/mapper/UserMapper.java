fb71c9f1bb6838801a645a7750b403d7d8be3e4f
刘海涛 <164755927@qq.com>
2023-04-06 22:24:05 +0800

    代码更改

--- src/main/java/com/outsourcing/mapper/UserMapper.java
+++ src/main/java/com/outsourcing/mapper/UserMapper.java
@@ -1,9 +1,12 @@
 package com.outsourcing.mapper;
 
 import com.outsourcing.pojo.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 public interface UserMapper {
 
@@ -17,4 +20,10 @@ public interface UserMapper {
     @Select("select * from tb_user where user_name=#{userName} and password=#{password} ")
     @ResultMap("userResultMap")
     User login(@Param("userName") String userName, @Param("password") String password);
+
+    @Select("select * from tb_user where user_name = #{userName}")
+    User selectByUserName(String userName);
+
+    @Insert("insert into tb_user values(null,#{userName},#{password},#{status})")
+    void add(User user);
 }
--- src/main/java/com/outsourcing/service/UserService.java
+++ src/main/java/com/outsourcing/service/UserService.java
@@ -2,8 +2,12 @@ package com.outsourcing.service;
 
 import com.outsourcing.pojo.User;
 
+import java.util.List;
+
 public interface UserService {
 
 
     User login(String userName,String password);
+
+    boolean register(User user);
 }
