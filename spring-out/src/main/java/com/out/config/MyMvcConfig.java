31d4d272c434869f4459daa8665edeaa2390cffd
Liu Haitao <164755927@qq.com>
2023-05-28 16:27:21 +0800

    SpringBoot更新界面

---
+++ spring-out/src/main/java/com/out/config/MyMvcConfig.java
@@ -0,0 +1,37 @@
+package com.out.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+        //用户路由
+        registry.addViewController("/login").setViewName("user/sign-in");
+        registry.addViewController("/logout").setViewName("user/sign-up");
+        //空白路由
+        registry.addViewController("/blank").setViewName("blank-page");
+        //3D操作路由
+        registry.addViewController("/importPictures").setViewName("3D_Operate/importPictures");
+        registry.addViewController("/viewResult").setViewName("3D_Operate/viewResult");
+        registry.addViewController("/processResult").setViewName("3D_Operate/processResult");
+        registry.addViewController("/historicalAnalysis").setViewName("3D_Operate/historicalAnalysis");
+        //医患操作
+        registry.addViewController("/checkRegistration").setViewName("patient/checkRegistration");
+        registry.addViewController("/processRecord").setViewName("patient/processRecord");
+        //医生操作
+        registry.addViewController("/checkCalendar").setViewName("work/checkCalendar");
+        registry.addViewController("/medicalConference").setViewName("work/medicalConference");
+        registry.addViewController("/otherRequests").setViewName("work/todo");
+        //治疗操作
+        registry.addViewController("/inHospital").setViewName("govern/inHospital");
+        registry.addViewController("/medicare").setViewName("govern/medicare");
+        registry.addViewController("/prescribe").setViewName("govern/prescribe");
+
+    }
+}
