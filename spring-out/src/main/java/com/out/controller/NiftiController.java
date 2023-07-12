6ffaa4a555c7089138d633c80da8c38e1b9f0660
Liu Haitao <164755927@qq.com>
2023-07-12 16:10:39 +0800

    Nifti图像测试

--- spring-out/src/main/java/com/out/controller/NiftiController.java
+++ spring-out/src/main/java/com/out/controller/NiftiController.java
@@ -1,28 +1,36 @@
 package com.out.controller;
 
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStream;
 
 @RestController
-@RequestMapping("/api")
 public class NiftiController {
 
-    @GetMapping(value = "/nifti", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public ResponseEntity<byte[]> getNiftiFile() throws IOException {
-        String filePath = "/home/lht/Code/pycharm/outsourcing/PaddleSeg/contrib/MedicalSeg/inference_model/output/test.nii.gz"; // 替换为你NIfTI文件的路径
-        Path path = Paths.get(filePath);
-        byte[] fileContent = Files.readAllBytes(path);
-        return ResponseEntity.ok().body(fileContent);
+    @GetMapping(value = "/api/nifti", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<InputStreamResource> getNiftiFile() throws IOException {
+        // 读取 NIFTI.GZ 文件内容
+        Resource resource = new ClassPathResource("D:\\21016\\Desktop\\outsourcing\\spring-out\\src\\main\\resources\\static\\nifti\\test.nii.gz");
+        InputStream inputStream = resource.getInputStream();
+
+        // 设置响应头信息
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=file.nii.gz");
+
+        // 返回文件内容
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .body(new InputStreamResource(inputStream));
     }
 }
+
 
